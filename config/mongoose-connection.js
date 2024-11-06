f87a1556cff0492fd081cf867cf5189c39e8eeb8
Aarti Mishra <aarti04mishra@gmail.com>
2024-11-06 22:45:09 +0530

    Initial commit

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,8 +1,14 @@
-const mongoose=require('mongoose');
-mongoose.connect(process.env.MONGO_DB_URI).then(function(){
-    console.log("connected to mongodb")
-})
+const mongoose = require('mongoose');
 
-let db=mongoose.connection;
+// Make sure the connection string is properly formatted
+mongoose.connect('mongodb+srv://aartimishra:asdfrgthyfrgxdef@cluster0.t5pow.mongodb.net/mykhatabook?retryWrites=true&w=majority')
+    .then(function() {
+        console.log("Connected to MongoDB");
+    })
+    .catch(function(error) {
+        console.log("Error connecting to MongoDB: ", error);
+    });
 
-module.exports =db;
+let db = mongoose.connection;
+
+module.exports = db;
